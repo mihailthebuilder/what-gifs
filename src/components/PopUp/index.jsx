b9851776d3db4518593740cf9d16bb294334009f
Mihail Marian <mihailmarian12@gmail.com>
2020-12-31 10:50:41 +0000

    styled the popup modal for desktop

--- src/components/PopUp/index.jsx
+++ src/components/PopUp/index.jsx
@@ -2,7 +2,7 @@ import "./PopUp.scss";
 
 import { MAX_SCORE } from "../../common/index.js";
 
-const PopUp = ({ show, message }) => {
+const PopUp = ({ show, messageType }) => {
   let popBackgClass = "popup-background";
   if (show) {
     popBackgClass += " show";
@@ -12,18 +12,23 @@ const PopUp = ({ show, message }) => {
   return (
     <div className={popBackgClass}>
       <div className="popup-wrapper">
-        <div className="popup-title">How it works</div>
+        <div className="popup-title font-size-large">How it works</div>
         <div className="popup-content font-size-regular">
           <p>
-            Welcome to the What GIFs memory game. The aim of the game is to make
-            sure you don't the same GIF in a given level.
+            Welcome to the <span className="frostbite-color">What GIFs</span>{" "}
+            memory game. The aim of the game is to make sure you don't
+            {desktopWindow ? " click" : " tap"} the same GIF in a given level.
           </p>
           <p>
             You can play the GIF by
             {desktopWindow ? " hovering over it" : " scrolling it into view"}.
-            The maximum score you can get is {MAX_SCORE}.
+            The maximum score you can get is{" "}
+            <span className="frostbite-color">{MAX_SCORE}</span>.
           </p>
         </div>
+        <button className="font-size-regular">
+          Understood, now let me play the damn game!
+        </button>
       </div>
     </div>
   );
